5f0d9c73847020a834a90c6495f2582bf83a1b7c
RJ Wright <rj@rjwright.me>
2020-10-24 15:06:30 +1100

    Add manager questions + validation with inquirer

--- app.js
+++ app.js
@@ -13,6 +13,97 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+/*
+
+
+Please build your team
+What is your manager's name?
+What is your manager's id? >>Please enter a positive number greater than zero.
+What is your manager's email? >>Please enter a valid email address
+What is your manager's office number?
+
+Which type of team member would you like to add?
+> Engineer
+> Intern
+> I'm done.
+
+What is your intern's name?
+What is your intern's id? >>This ID is already taken. Please use a unique ID.
+What is your intern's email address?
+What is your intern's school?
+>> Intern Added. (Jump back to new team member menu above)
+
+What is your engineer's name?
+What is your engineer's id?
+What is your engineer's email address?
+What is your engineer's GitHub user name?
+>> Engineer Added. (Jump back to new team member menu above)
+
+(after form completion)
+Team file written. You can view it here: XXXXXXXXX
+
+*/
+
+// Validation functions
+let usedIDs = [];
+const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+const idValidator = async (input) => {
+    if (/[^0-9]/.test(input)) {
+        return 'Please enter a number.'
+    } else {
+        for (const id in usedIDs) {
+            if (id === input) {
+                return 'This ID is already taken. Please use a unique ID.';
+            }
+        }
+        usedIDs.push(input);
+        return true;
+    }
+};
+
+const numberValidator = async (input) => {
+    return /[^0-9]/.test(input) ? 'Please enter a number.' : true;
+}
+
+const emailValidator = async (input) => {
+    return emailRegEx.test(input) ? true : 'Please enter a valid email address';
+}
+
+// Inquirer Prompts
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is your manager's name?",
+            default: "Mr Manager",
+        },
+        {
+            type: "input",
+            name: "managerID",
+            message: "What is your manager's id?",
+            default: "1234",
+            validate: numberValidator,
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is your manager's email?",
+            default: "example@mail.com",
+            validate: emailValidator,
+        },
+        {
+            type: "input",
+            name: "managerOfficeNumber",
+            message: "What is your manager's office number?",
+            default: "1234",
+            validate: numberValidator,
+        },
+    ])
+    .then( answers => console.log(answers) )
+    .catch( error => console.error(error) );
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -24,12 +115,3 @@ const render = require("./lib/htmlRenderer");
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
